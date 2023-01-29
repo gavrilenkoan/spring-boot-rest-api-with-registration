9d1128276432c4c807278f9e380170a7dc6e2b23
gavrilenko_an <havrilenko.ann@gmail.com>
2023-01-29 13:21:47 +0200

    SecurityConfig.java - renamed from WebSecurityConfig.java

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class WebSecurityConfig {
+public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
